805f7aa4902fe3b9ada98fae1b243cda67feb62e
VINAY-SANDEEP <rsrinivasa782@gmail.com>
2025-05-30 17:41:48 +0530

    hh

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,11 @@ import ChatBot from "../components/ChatBot";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white flex items-center justify-center">
-      <ChatBot />
-    </main>
+    <main className="min-h-screen bg-white relative">
+  <div className="absolute top-10 left-10">
+    <ChatBot />
+  </div>
+</main>
+
   );
 }
